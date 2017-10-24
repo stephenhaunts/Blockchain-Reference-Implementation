0649b9c1816c32b8c21ec84014e440409807ffed
stephen haunts <stephen.haunts@outlook.com>
2017-10-24 21:54:35 +0100

    Add projects for Module 3

---
+++ BlockWithMultipleTransactions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlockWithMultipleTransactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
