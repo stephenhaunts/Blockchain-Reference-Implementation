4004dae3e5742f28ac1563862223e207fee5a234
stephen haunts <stephen.haunts@outlook.com>
2017-10-26 10:03:20 +0100

    Add method skeletons to calculate hashes and assign next block

--- BlockWithSingleTransaction/Block.cs
+++ BlockWithSingleTransaction/Block.cs
@@ -16,9 +16,14 @@ namespace BlockChainCourse.BlockWithSingleTransaction
         int Mileage { get; set; }
         ClaimType ClaimType { get; set; }
 
+        int BlockNumber { get; }
         DateTime CreatedDate { get; }
         string BlockHash { get; }
-        string PreviousBlockHash { get; }
+        string PreviousBlockHash { get; set; }
+
+        string CalculateBlockHash();
+        void SetBlockHash();
+        IBlock NextBlock { get; }
     }
 
     public class Block : IBlock
@@ -32,16 +37,49 @@ namespace BlockChainCourse.BlockWithSingleTransaction
         public ClaimType ClaimType { get; set; }
 
         // Set as part of the block creation process.
+        public int BlockNumber { get; private set; }
         public DateTime CreatedDate { get; private set; }
         public string BlockHash { get; private set; }
-        public string PreviousBlockHash { get; private set; }
+        public string PreviousBlockHash { get; set; }
+        public IBlock NextBlock { get; private set; }
 
-        public Block()
+        public Block(string claimNumber,
+                     decimal settlementAmount,
+                     DateTime settlementDate,
+                     string carRegistration,
+                     int mileage,
+                     ClaimType claimType)
         {
-         
+            ClaimNumber = claimNumber;
+            SettlementAmount = settlementAmount;
+            SettlementDate = settlementDate;
+            CarRegistration = carRegistration;
+            Mileage = mileage;
+            ClaimType = claimType;
         }
 
-     
+        // Calculate and return the block hash
+        public string CalculateBlockHash()
+        {
+            return "";
+        }
 
+        // Set the block hash
+        public void SetBlockHash()
+        {
+            BlockHash = CalculateBlockHash();
+        }
+
+        public void SetNextBlock (IBlock nextBlock)
+        {
+            // Assign next block
+            NextBlock = nextBlock;
+
+            // Calculate the hash for this block
+            SetBlockHash();
+
+            // Then assign it to the previous block hash of the next block in the chain.
+            NextBlock.PreviousBlockHash = BlockHash;
+        }
     }
 }
