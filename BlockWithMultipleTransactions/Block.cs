598017ac395e24d42d10d5ed1020273342d08a6e
stephen haunts <stephen.haunts@outlook.com>
2017-11-06 16:02:50 +0000

    Forgot to check in block class changes. oops

--- BlockWithMultipleTransactions/Block.cs
+++ BlockWithMultipleTransactions/Block.cs
@@ -4,37 +4,6 @@ using BlockChainCourse.Cryptography;
 
 namespace BlockChainCourse.BlockWithMultipleTransactions
 {
-    public class Transaction : ITransaction
-    {
-        public string ClaimNumber { get; set; }
-        public decimal SettlementAmount { get; set; }
-        public DateTime SettlementDate { get; set; }
-        public string CarRegistration { get; set; }
-        public int Mileage { get; set; }
-        public ClaimType ClaimType { get; set; }
-
-        public Transaction(string claimNumber,
-                            decimal settlementAmount,
-                            DateTime settlementDate,
-                            string carRegistration,
-                            int mileage,
-                            ClaimType claimType)
-        {
-            ClaimNumber = claimNumber;
-            SettlementAmount = settlementAmount;
-            SettlementDate = settlementDate;
-            CarRegistration = carRegistration;
-            Mileage = mileage;
-            ClaimType = claimType;
-        }
-
-        public string CalculateTransactionHash()
-        {
-            string txnHash = ClaimNumber + SettlementAmount + SettlementDate + CarRegistration + Mileage + ClaimType;
-            return Convert.ToBase64String(HashData.ComputeHashSha256(Encoding.UTF8.GetBytes(txnHash)));
-        }
-    }
-
     public class Block : IBlock
     {
         public ITransaction Transaction { get; set; }
