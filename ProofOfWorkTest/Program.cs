a8b2ba60c9f2f6f2b7f63d85fee779a7f296273c
stephen haunts <stephen.haunts@outlook.com>
2017-11-06 18:57:49 +0000

    Add Proof of Work testin project

---
+++ ProofOfWorkTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlockChainCourse.ProofOfWorkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
